4f8385bb92c903eb0d39edb0926ed15341237806
Riley Boyd <riley.boyd@catoddb-rboyd.ad.ddb.ca>
2018-04-16 11:27:20 -0400

    Made my own demo, created a custom Muffin object, added a method to the prototype that says the type of muffin when called.

--- ch-01/lib/js/app.js
+++ ch-01/lib/js/app.js
@@ -3,7 +3,39 @@
 /**********************/
 
 window.onload = function() {
+
+	// MUFFIN OBJECT CONSTRUCTOR
+	function Muffin(type,sugar,isWholeWheat,isGlutenFree) {
+		this.type = type; // Name of muffin
+		this.sugar = sugar; // Amount of sugar in grams
+		this.isWholeWheat = isWholeWheat; // Boolean: Is it whole wheat?
+		this.isGlutenFree = isGlutenFree; // Boolean: Is it gluten free?
+	}
 	
-	console.log("testing");
+	Muffin.prototype = {
+		constructor: Muffin,
+		sayType: function() {
+			alert(this.type);
+		}
+	}
+
+	// CREATE A MUFFIN
+	var muffin1 = new Muffin('Chocolate Chip',33,true,false);
+
+	console.log(muffin1);
+
+	// When muffin button is clicked, say what type of muffin it is
+	var btns = document.getElementsByClassName('btn');
+
+	for (var i=0;i<btns.length;i++) {
+
+		btns[i].addEventListener('click', function(event){
+
+			event.preventDefault();
+			muffin1.sayType();
+
+		});
+
+	}
 
 }
